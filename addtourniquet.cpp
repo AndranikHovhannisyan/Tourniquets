8ddfce0207b30d2ad3bd5a8efd7dfae8af294b4c
andranik <An.corp@mail.ru>
2015-03-19 07:46:43 +0400

    tournicet

--- addtourniquet.cpp
+++ addtourniquet.cpp
@@ -7,9 +7,13 @@ addTourniquet::addTourniquet(QSqlDatabase db, QWidget *parent) :
     ui(new Ui::addTourniquet)
 {
     ui->setupUi(this);
-    ui->type->addItem("In");
-    ui->type->addItem("Out");
-    this->setWindowTitle("add tourniquet");
+    this->setWindowTitle("Ավելացնել տուրնիկետ");
+    tableName = "tourniquet";
+
+    ui->type->insertItem(0, "Մուտք");
+    ui->type->insertItem(1, "Ելք");
+
+    connect(ui->buttonBox, SIGNAL(accepted()), this, SLOT(create()));
 }
 
 addTourniquet::~addTourniquet()
@@ -17,14 +21,19 @@ addTourniquet::~addTourniquet()
     delete ui;
 }
 
-void addTourniquet::init(QSqlRecord &) {
-    qDebug() << "id =" << id;
+void addTourniquet::init(QSqlRecord &record) {
+    ui->type->setCurrentIndex(record.value("type").toInt());
+    ui->number->setText(record.value("number").toString());
 }
 
 void addTourniquet::claer() {
-
+    ui->type->setCurrentIndex(0);
+    ui->number->setText("");
 }
 
-void addTourniquet::populateData(QSqlRecord &rec) {
+void addTourniquet::populateData(QSqlRecord &record) {
+    qDebug() << ui->type->currentIndex();
 
+    record.setValue(record.indexOf("type"), QVariant(ui->type->currentIndex()));
+    record.setValue(record.indexOf("number"), QVariant(ui->number->text()));
 }
