30136946c6635f9cfc84d800ff4d06fa7f104828
andranik <An.corp@mail.ru>
2015-03-19 07:36:54 +0400

    schedule type create edit

--- addscheduletype.cpp
+++ addscheduletype.cpp
@@ -7,7 +7,10 @@ addScheduleType::addScheduleType(QSqlDatabase db, QWidget *parent) :
     ui(new Ui::addScheduleType)
 {
     ui->setupUi(this);
-    this->setWindowTitle("add schedule type");
+    this->setWindowTitle("Ավելացնել գրաֆիկի տեսակ");
+    tableName = "schedule_type";
+
+    connect(ui->buttonBox, SIGNAL(accepted()), this, SLOT(create()));
 }
 
 addScheduleType::~addScheduleType()
@@ -15,14 +18,18 @@ addScheduleType::~addScheduleType()
     delete ui;
 }
 
-void addScheduleType::init(QSqlRecord &) {
-    qDebug() << "id =" << id;
+void addScheduleType::init(QSqlRecord &record) {
+    ui->name->setText(record.value("name").toString());
+    ui->allowLag->setTime(record.value("allow_lag").toTime());
+    ui->ignoreTime->setTime(record.value("ignore_time").toTime());
 }
 
 void addScheduleType::claer() {
-
+    ui->name->setText("");
 }
 
-void addScheduleType::populateData(QSqlRecord &rec) {
-
+void addScheduleType::populateData(QSqlRecord &record) {
+    record.setValue(record.indexOf("name"), QVariant(ui->name->text()));
+    record.setValue(record.indexOf("allow_lag"), QVariant(ui->allowLag->time()));
+    record.setValue(record.indexOf("ignore_time"), QVariant(ui->ignoreTime->time()));
 }
